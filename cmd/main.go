b19e6195a2ca1430ec694963d35ad539777059b7
nicklanng <rinse@rinseworld.com>
2017-06-08 20:33:06 +0100

    Specify a port to listen on

--- cmd/main.go
+++ cmd/main.go
@@ -5,23 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/nicklanng/gecho/pkg"
 )
 
 func main() {
-	service := getConfig()
+	service, port := getConfig()
+
+	portString := strconv.Itoa(port)
 
 	http.HandleFunc("/", pkg.Handler(service))
-	fmt.Println("listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("listening on :" + portString)
+	log.Fatal(http.ListenAndServe(":"+portString, nil))
 }
 
-func getConfig() (service string) {
+func getConfig() (service string, port int) {
 	servicePtr := flag.String("service", "gecho", "A service name to be returned in the responses")
+	portPtr := flag.Int("port", 8443, "Which port to listen on")
 	flag.Parse()
 
 	service = *servicePtr
+	port = *portPtr
 
 	return
 }
