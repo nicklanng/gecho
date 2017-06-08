cb924ba074c15a3352b7bc7131783f8be3b2529c
nicklanng <rinse@rinseworld.com>
2017-06-08 20:53:44 +0100

    Can specify SSL mode

--- cmd/main.go
+++ cmd/main.go
@@ -11,22 +11,34 @@ import (
 )
 
 func main() {
-	service, port := getConfig()
+	service, certPath, keyPath, port, ssl := getConfig()
 
 	portString := strconv.Itoa(port)
 
 	http.HandleFunc("/", pkg.Handler(service))
 	fmt.Println("listening on :" + portString)
-	log.Fatal(http.ListenAndServe(":"+portString, nil))
+
+	if ssl {
+		log.Fatal(http.ListenAndServeTLS(":"+portString, certPath, keyPath, nil))
+	} else {
+		log.Fatal(http.ListenAndServe(":"+portString, nil))
+	}
 }
 
-func getConfig() (service string, port int) {
+func getConfig() (service, certPath, keyPath string, port int, ssl bool) {
 	servicePtr := flag.String("service", "gecho", "A service name to be returned in the responses")
-	portPtr := flag.Int("port", 8443, "Which port to listen on")
+	certPathPtr := flag.String("cert", "server.crt", "The certificate for server SSL")
+	keyPathPtr := flag.String("key", "server.key", "The key for server SSL")
+	portPtr := flag.Int("port", 8080, "Which port to listen on")
+	sslPtr := flag.Bool("ssl", false, "Use SSL. Recommend also set the cert and key args.")
+
 	flag.Parse()
 
 	service = *servicePtr
+	certPath = *certPathPtr
+	keyPath = *keyPathPtr
 	port = *portPtr
+	ssl = *sslPtr
 
 	return
 }
